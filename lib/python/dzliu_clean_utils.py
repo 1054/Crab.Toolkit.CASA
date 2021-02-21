1944a8083e3638bd4da4711a1cbc0ba179dbdb2d
1054 <>
2021-02-21 03:59:35 +0100

    Updates

--- lib/python/dzliu_clean_utils.py
+++ lib/python/dzliu_clean_utils.py
@@ -350,6 +350,42 @@ def get_mosaic_imsize_and_phasecenter(vis, cell, galaxy_name='', ref_freq_Hz=Non
 
 
 
+# 
+# get uvdist
+# 
+def get_synbeam_and_imcell(vis, ref_freq_Hz = None, oversampling = 5.0):
+    # 
+    tb.open(vis)
+    uvw = tb.getcol('UVW') # shape (3, nrows), each row is a (u,v,w) array
+    ##print2('tb.query(\'FIELD_ID in [%s] AND DATA_DESC_ID in [%s] AND STATE_ID in [%s]\', \'UVW\')'%(','.join(matched_field_indices.astype(str)), ','.join(valid_data_desc_indicies.astype(str)), ','.join(valid_state_indices.astype(str))))
+    ##result = tb.query('FIELD_ID in [%s] AND DATA_DESC_ID in [%s] AND STATE_ID in [%s]'%(','.join(matched_field_indices.astype(str)), ','.join(valid_data_desc_indicies.astype(str)), ','.join(valid_state_indices.astype(str))), 'UVW')
+    #print2('tb.query(\'FIELD_ID in [%s] AND STATE_ID in [%s]\', \'UVW\')'%(','.join(matched_field_indices.astype(str)), ','.join(valid_state_indices.astype(str))))
+    #result = tb.query('FIELD_ID in [%s] AND STATE_ID in [%s]'%(','.join(matched_field_indices.astype(str)), ','.join(valid_state_indices.astype(str))), 'UVW')
+    #uvw = result.getcol('UVW')
+    tb.close()
+    # 
+    if ref_freq_Hz is None:
+        ref_freq_Hz = get_ref_frequency(vis)
+    # 
+    uvdist = np.sqrt(np.sum(np.square(uvw[0:2, :]), axis=0))
+    maxuvdist = np.max(uvdist)
+    print2('maxuvdist = %s [m]'%(maxuvdist))
+    L80uvdist = np.percentile(uvdist, 80) # np.max(uvdist) # now I am using 90-th percentile of baselies, same as used by 'analysisUtils.py' pickCellSize() getBaselineStats(..., percentile=...)
+    print2('L80uvdist = %s [m] (80-th percentile)'%(L80uvdist))
+    # 
+    synbeam = 2.99792458e8 / ref_freq_Hz / maxuvdist / np.pi * 180.0 * 3600.0 # arcsec
+    synbeam = 0.574 * 2.99792458e8 / ref_freq_Hz / L80uvdist / np.pi * 180.0 * 3600.0 # arcsec # .574lambda/L80, see 'analysisUtils.py' estimateSynthesizedBeamFromASDM()
+    synbeam_nprec = 3 # keep 3 valid digits
+    synbeam_ndigits = (synbeam_nprec-1) - int(np.floor(np.log10(synbeam))) # keep to these digits (precision) after point, e.g., 0.1523 -> nprec 2 -> round(0.1523*100)/100 = 0.15
+    synbeam = (np.round(synbeam * 10**(synbeam_ndigits))) / 10**(synbeam_ndigits)
+    #oversampling = 5.0
+    imcell_arcsec = synbeam / oversampling
+    imcell = '%sarcsec'%(imcell_arcsec)
+    print2('synbeam = %s [arcsec]'%(synbeam))
+    print2('imcell_arcsec = %s'%(imcell_arcsec))
+    return synbeam, imcell
+
+
 #
 # def get spw for spectral line
 #
